fe2bcb56112996813f8f54f546547f91e09d835c
Виталий Станкевич <vitaliy.v.stankevich>
2022-11-02 22:10:29 +0200

    Make everythink

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.shortcuts import get_object_or_404
 
 from posts.models import Group, Post
 from .permissions import IsOwnerOrReadOnly
